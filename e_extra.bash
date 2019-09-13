df23f6ada9cd4159e8469c66652054dc5a8e5325
Stuart Wong <cgs.wong@gmail.com>
2019-09-13 15:35:28 -0500

    chore: add serverless

--- e_extra.bash
+++ e_extra.bash
@@ -104,7 +104,10 @@ if which direnv &> /dev/null; then  eval "$(direnv hook ${SHELL_NAME})"; fi
 ## Serverless
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
-[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.${SHELL_NAME} ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.${SHELL_NAME}
+[ -f /Users/carrwong/node_modules/tabtab/.completions/serverless.${SHELL_NAME} ] && . /Users/carrwong/node_modules/tabtab/.completions/serverless.${SHELL_NAME}
 # tabtab source for sls package
 # uninstall by removing these lines or running `tabtab uninstall sls`
-[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.${SHELL_NAME} ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.${SHELL_NAME}
+[ -f /Users/carrwong/node_modules/tabtab/.completions/sls.${SHELL_NAME} ] && . /Users/carrwong/node_modules/tabtab/.completions/sls.${SHELL_NAME}
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /Users/carrwong/node_modules/tabtab/.completions/slss.${SHELL_NAME} ] && . /Users/carrwong/node_modules/tabtab/.completions/slss.${SHELL_NAME}
