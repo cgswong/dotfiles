765206266bc7872174ba5f64db9c0036028cd97e
Stuart Wong <cgs.wong@gmail.com>
2020-04-14 12:10:51 -0500

    chore: use ZSH plugins instead of aliases

--- aliases.bash
+++ aliases.bash
@@ -1,29 +1,4 @@
-# Shortcuts
-alias g="git"
-alias h="history"
-alias j="jobs"
-
-# Set ls color flag
-# export CLICOLOR=1
-# export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-
-# if ls --version &>/dev/null ; then
-#   colorflag="--color=auto"
-# else
-# Legacy macOS
-#   colorflag="-C"
-# fi
-
-# Always use color output for `ls`
-# alias ls="command ls ${colorflag}"
-
 # List all files colorized
-# alias l="ls -Fhp"
-# alias ll="ls -lFhp"
-
-# List all files colorized in long format, including dot files
-# alias la="ls -laFhp"
-
 # List only directories
 alias lsd="ls -lFhp | grep --color=never '^d'"
 
@@ -75,17 +50,10 @@ alias trimcp="tr -d '\n' | pbcopy"
 [[ $(uname -a) != "Linux"* ]] && alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 
 if [[ $(uname -a) != "Linux"* ]]; then
-  # Show/hide hidden files in Finder
-  alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-  alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
   # Hide/show all desktop icons (useful when presenting)
   alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
   alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
 fi
-
-# URL-encode strings
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
 
 # Merge PDF files
 # Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
